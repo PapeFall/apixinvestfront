2d5acbf44b9fc8c1de94cd123e6130dcb37d6f5d
PapeFall <epape1100@gmail.com>
2022-03-31 00:15:36 +0000

    user ok

--- src/app/dashboard/components/utilisateur-list/utilisateur-list.component.ts
+++ src/app/dashboard/components/utilisateur-list/utilisateur-list.component.ts
@@ -7,17 +7,15 @@ import { MatTableDataSource } from '@angular/material/table';
   styleUrls: ['./utilisateur-list.component.scss']
 })
 export class UtilisateurListComponent implements OnInit {
-  displayedColumns: string[] = ['username', 'email', 'profil', 'action'];
+  displayedColumns: string[] = ['username', 'email', 'profil','etat', 'action'];
   dataSource:MatTableDataSource<any> = new MatTableDataSource<any>([
-    {
-      username: "Aliou Ndiaye",
-      email: "admin@gmail.com",
-      profil: "admin"
-    },
-    { username: 'Joseph Sarr', profil: 'partenaire',email:'partenaire@gmail.com' },
-    { username: 'Fatim Wade', profil: 'intervenant',email:'intervenant@gmail.com' },
-    { username: 'Moussa Diop', profil: 'participant',email:'participant@gmail.com' }
+    { username: "Aliou Ndiaye", profil: "admin",etat:true, email: "admin@gmail.com"},
+    { username: 'Joseph Sarr', profil: 'partenaire',etat:true,email:'partenaire@gmail.com' },
+    { username: 'Fatim Wade', profil: 'intervenant',etat:true,email:'intervenant@gmail.com' },
+    { username: 'Moussa Diop', profil: 'participant',etat:false,email:'participant@gmail.com' }
   ])
+  profils =['admin','partenaire','intervenant','participant'];
+  etats = ['Actif','Inactif'];
   constructor() { }
 
   ngOnInit(): void {
