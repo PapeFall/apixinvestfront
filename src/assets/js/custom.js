a45f2cd8851b0aa9258b04fb32a77d2f96305235
PapeFall <epape1100@gmail.com>
2022-04-08 15:59:04 +0000

    cover ok

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -96,7 +96,7 @@
                 minSlides: 1,
                 maxSlides: 6,
                 slideWidth: 200,
-                slideMargin: 30,
+                slideMargin: 10,
                 ticker: true,
                 speed: 30000
             });
