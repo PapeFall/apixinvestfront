dd1b0f49ca1777779b4c37d5eb34dfb29e97ac99
PapeFall <epape1100@gmail.com>
2022-04-17 13:50:31 +0000

    home integration ok

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -13,15 +13,37 @@ export class HomeComponent implements OnInit {
 
   constructor(private articleService: ArticlesService, private projetService: ProjetsService) { 
     new Promise(resolve =>{
-      this.loadScript()
+      this.scripts.forEach((script)=>this.loadScript(script))
+      // this.loadScript()
     })
     this.getArticles();
     this.getProjets();
   }
-
-  public loadScript(){
+  scripts = [
+    "assets/apix2/js/revolution-slider/js/extensions/revolution.extension.actions.min.js",
+  
+    "assets/apix2/js/revolution-slider/js/extensions/revolution.extension.carousel.min.js",
+  
+    "assets/apix2/js/revolution-slider/js/extensions/revolution.extension.kenburn.min.js",
+  
+    "assets/apix2/js/revolution-slider/js/extensions/revolution.extension.layeranimation.min.js",
+  
+    "assets/apix2/js/revolution-slider/js/extensions/revolution.extension.migration.min.js",
+  
+    "assets/apix2/js/revolution-slider/js/extensions/revolution.extension.navigation.min.js",
+  
+    "assets/apix2/js/revolution-slider/js/extensions/revolution.extension.parallax.min.js",
+  
+    "assets/apix2/js/revolution-slider/js/extensions/revolution.extension.slideanims.min.js",
+  
+    "assets/apix2/js/revolution-slider/js/extensions/revolution.extension.video.min.js",
+    'assets/apix2/js/custom.js',
+    'assets/apix2/js/custom2home.js'
+  ]
+  public loadScript(script:string){
     const node = document.createElement('script');
-    node.src = 'assets/js/custom.js';
+    // node.src =     'assets/apix2/js/custom.js';
+    node.src = script
     node.type='text/javascript';
     node.async=true;
     node.charset='utf-8';
